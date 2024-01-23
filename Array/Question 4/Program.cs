f3629d963f7a33a3d8dc6b20092a9b25e35648e1
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 08:35:00 +0530

    Array_Finding Max and Min Element  in array

---
+++ Array/Question 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 89, 23, };
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"MAX is {max}");
+            Console.WriteLine($"MIN is {min}");
+            Console.ReadLine();
+        }
+    }
+}
