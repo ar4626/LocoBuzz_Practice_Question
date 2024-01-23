f1a9e759858fd601a56f4808c4e1e6c469337a2a
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 08:55:23 +0530

    Array_Reverse a string

---
+++ Array/Question 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Ankit";
+            Console.WriteLine(s);
+            char[] chars = s.ToCharArray();
+            for(int i = 0, j=s.Length-1; i < j; i++, j--)
+            {
+                char c = chars[i];
+                chars[i] = s[j];
+                chars[j] = c;
+            }
+            Console.WriteLine(chars);
+            Console.ReadLine();
+        }
+    }
+}
