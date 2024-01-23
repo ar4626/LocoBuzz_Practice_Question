65e660dfef30404a2ece3c6262d15d7009e4da73
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 08:48:24 +0530

    Array_Printing a reectangular star pattern

---
+++ Array/Question 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            for(int i = 0; i < n; i++)
+            {
+                if(i==0 || i == n-1)
+                {
+                    for(int j= 0; j < n; j++)
+                    {
+                        Console.Write("* ");
+                    }
+                }
+                else
+                {
+                    Console.Write("*         *");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
