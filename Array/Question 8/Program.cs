34e52362e47275c4a7ee612d2b400ba6c6b33776
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 10:02:06 +0530

    Array_Finding Sum of digit of a number

---
+++ Array/Question 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 234534;
+            int sum = 0;
+            for(int i = n;n>0; i++)
+            {
+                sum = sum + n%10;
+                n = n / 10;
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
