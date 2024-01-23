8cc73bdbbf686e489993ca847df0e331f8a2f848
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 08:42:00 +0530

    Array_Making a pattern for the stars

---
+++ Array/Question 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            for(int i = n; i > 0; i--)
+            {
+                for(int j = i; j > 0; j--)
+                {
+                    Console.Write($" *");
+                }
+                Console.WriteLine();
+                n--;
+            }
+            Console.ReadLine();
+        }
+    }
+}
