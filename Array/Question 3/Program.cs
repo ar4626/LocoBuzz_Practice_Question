b9f1de59668b69f93f94560292e6be43e42b999f
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 08:29:16 +0530

    Array_Printing Frequency of Each Element

---
+++ Array/Question 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 3, 4, 2, 1, 5, 8 };
+            int count;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                count = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"{arr[i]} is {count} times;");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
