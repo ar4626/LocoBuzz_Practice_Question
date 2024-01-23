148e4e3ad6e2ed06d4e341fc8577fb98a730e2e6
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 08:18:18 +0530

    Array_Finding Unique Element in an array

---
+++ Array/Question 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 2, 3, 3, 4, 4, 4, 5, 7 };
+            int count = 0;
+
+            for(int i = 0;i<arr.Length;i++)
+            {
+                count = 0;
+                for(int j = 0;j<arr.Length;j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                if(count == 1)
+                {
+
+                Console.WriteLine($"{arr[i]} is {count} times.");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
