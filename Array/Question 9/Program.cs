0deb8d43bc56109650addd60936094f3815d5e04
Ankit Raj <ankitsingh17072001@gmail.com>
2024-01-23 09:50:37 +0530

    Array_Finding Sum of Rows of matrix

---
+++ Array/Question 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9, } };
+
+            int sum = 0;
+            for(int i=0;i<arr.GetLength(0);i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    sum += arr[i,j];
+                }
+                Console.WriteLine($"Sum of {i+1}th row is {sum}");
+                sum = 0;
+            }
+            Console.ReadLine();
+        }
+    }
+}
